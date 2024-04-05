122411592e0682c2cbf942f542bab17dff5d2a37
okikohaku <gtracer465@gmail.com>
2024-04-05 15:53:08 -0400

    Finish tests 3, 4

--- testingFinal.java
+++ testingFinal.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -20,14 +21,55 @@ public class testingFinal { /*
         driver.findElement(By.xpath("//*[@id=\"Header_GlobalLogin_WC_AccountDisplay_FormInput_logonPassword_In_Logon_1\"]"))
                 .sendKeys("GeorgeForeman1", Keys.ENTER);
         Thread.sleep(10000);
-    } */
+    }
 
     @Test(priority = 2)
     public void categoriesTest() throws InterruptedException {
         // Init driver
         WebDriver driver = new FirefoxDriver();
         driver.get("http://basspro.com");
+        Thread.sleep(2500);
+
+        // Cycle through four different categories
+        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732393\"]")).click(); // Fishing
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732410\"]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732421\"]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732432\"]")).click();
         Thread.sleep(1500);
 
+        driver.quit();
     }
+
+    @Test(priority = 3)
+    public void subcategoryTest() throws InterruptedException {
+        // Init driver
+        WebDriver driver = new FirefoxDriver();
+        driver.get("http://basspro.com");
+        driver.manage().window().maximize();
+        Thread.sleep(2500);
+
+        // Click fishing category, then go to fishing subcategories (fishing main page)
+        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732393\"]")).click();
+        Thread.sleep(250);
+        driver.findElement(By.xpath("//*[@id=\"departmentLink_3074457345616732393\"]")).click();
+        Thread.sleep(2500);
+
+        // Find and accept cookies warning so that subcategories are clickable
+        driver.findElement(By.xpath("//*[@id=\"CookieTrackingDisclosureButton\"]")).click();
+
+        // Click one sub category, back, then to another
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[3]/div/div[1]/div")).click();
+        Thread.sleep(4000);
+        driver.navigate().back();
+        Thread.sleep(2500);
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[3]/div/div[2]/div")).click();
+        Thread.sleep(4000);
+
+        driver.quit();
+    } */
+
+
 }
