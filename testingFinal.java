e9d5c417d958edf237da781b39d5982f50b6a33f
okikohaku <gtracer465@gmail.com>
2024-04-07 11:35:52 -0400

    Change login routine to target dedicated login page

--- testingFinal.java
+++ testingFinal.java
@@ -14,14 +14,18 @@ public class testingFinal {
         driver.manage().window().maximize();
         Thread.sleep(1500);
 
-        // Click account button, submit credentials and login
-        driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[3]/div[1]/div[1]/div[3]/div/div[2]/div/div[1]/div/div[1]")).click();
-        Thread.sleep(2000);
-        driver.findElement(By.id("Header_GlobalLogin_WC_AccountDisplay_FormInput_logonId_In_Logon_1"))
+        // Click club, then click manage account
+        driver.findElement(By.id("clubLink")).click();
+        Thread.sleep(2500);
+        driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div/div/div/div[2]/div/div/div/nav/ul/li[3]")).click();
+        Thread.sleep(2500);
+
+        // Send keys to login boxes
+        driver.findElement(By.id("WC_AccountDisplay_FormInput_logonId_In_Logon_1"))
                 .sendKeys("spamforpixelspark@gmail.com");
-        driver.findElement(By.id("Header_GlobalLogin_WC_AccountDisplay_FormInput_logonPassword_In_Logon_1"))
+        driver.findElement(By.id("WC_AccountDisplay_FormInput_logonPassword_In_Logon_1"))
                 .sendKeys("GeorgeForeman1", Keys.ENTER);
-        Thread.sleep(10000);
+        Thread.sleep(5000);
 
         driver.quit();
     }
