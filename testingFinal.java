e91a4716b117a1c47d5350637d1963fb53848019
okikohaku <gtracer465@gmail.com>
2024-04-07 10:48:25 -0400

    Clean up xpaths that only contain ids

--- testingFinal.java
+++ testingFinal.java
@@ -17,11 +17,13 @@ public class testingFinal {
         // Click account button, submit credentials and login
         driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[3]/div[1]/div[1]/div[3]/div/div[2]/div/div[1]/div/div[1]")).click();
         Thread.sleep(2000);
-        driver.findElement(By.xpath("//*[@id=\"Header_GlobalLogin_WC_AccountDisplay_FormInput_logonId_In_Logon_1\"]"))
+        driver.findElement(By.id("Header_GlobalLogin_WC_AccountDisplay_FormInput_logonId_In_Logon_1"))
                 .sendKeys("spamforpixelspark@gmail.com");
-        driver.findElement(By.xpath("//*[@id=\"Header_GlobalLogin_WC_AccountDisplay_FormInput_logonPassword_In_Logon_1\"]"))
+        driver.findElement(By.id("Header_GlobalLogin_WC_AccountDisplay_FormInput_logonPassword_In_Logon_1"))
                 .sendKeys("GeorgeForeman1", Keys.ENTER);
         Thread.sleep(10000);
+
+        driver.quit();
     }
 
     @Test(priority = 2)
@@ -32,13 +34,13 @@ public class testingFinal {
         Thread.sleep(2500);
 
         // Cycle through four different categories
-        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732393\"]")).click(); // Fishing
+        driver.findElement(By.id("departmentButton_3074457345616732393")).click(); // Fishing
         Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732410\"]")).click();
+        driver.findElement(By.id("departmentButton_3074457345616732410")).click();
         Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732421\"]")).click();
+        driver.findElement(By.id("departmentButton_3074457345616732421")).click();
         Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732432\"]")).click();
+        driver.findElement(By.id("departmentButton_3074457345616732432")).click();
         Thread.sleep(1500);
 
         driver.quit();
@@ -53,13 +55,13 @@ public class testingFinal {
         Thread.sleep(2500);
 
         // Click fishing category, then go to fishing subcategories (fishing main page)
-        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732393\"]")).click();
+        driver.findElement(By.id("departmentButton_3074457345616732393")).click();
         Thread.sleep(250);
-        driver.findElement(By.xpath("//*[@id=\"departmentLink_3074457345616732393\"]")).click();
+        driver.findElement(By.id("departmentLink_3074457345616732393")).click();
         Thread.sleep(2500);
 
         // Find and accept cookies warning so that subcategories are clickable
-        driver.findElement(By.xpath("//*[@id=\"CookieTrackingDisclosureButton\"]")).click();
+        driver.findElement(By.id("CookieTrackingDisclosureButton")).click();
 
         // Click one sub category, back, then to another
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[3]/div/div[1]/div")).click();
@@ -81,20 +83,20 @@ public class testingFinal {
         Thread.sleep(2500);
 
         // Click fishing category, then go to fishing subcategories (fishing main page)
-        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732393\"]")).click();
+        driver.findElement(By.id("departmentButton_3074457345616732393")).click();
         Thread.sleep(250);
-        driver.findElement(By.xpath("//*[@id=\"departmentLink_3074457345616732393\"]")).click();
+        driver.findElement(By.id("departmentLink_3074457345616732393")).click();
         Thread.sleep(2500);
 
         // Find and accept cookies warning so that subcategories are clickable
-        driver.findElement(By.xpath("//*[@id=\"CookieTrackingDisclosureButton\"]")).click();
+        driver.findElement(By.id("CookieTrackingDisclosureButton")).click();
 
         // Click power reels category
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[3]/div/div[1]/div")).click();
         Thread.sleep(2500);
 
         // Filter to spinning reels, then sort by price descending
-        driver.findElement(By.xpath("//*[@id=\"maxavailquantity_In Stock\"]")).click();
+        driver.findElement(By.id("maxavailquantity_In Stock")).click();
         Thread.sleep(1000);
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div/div[4]/div[2]/div[1]/div[4]/div/div/div[1]")).click();
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div/div[4]/div[2]/div[1]/div[4]/div/div/div[2]/div[5]")).click();
@@ -115,7 +117,7 @@ public class testingFinal {
         // Click store selector, input zip code, save store
         driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div[1]")).click();
         Thread.sleep(500);
-        driver.findElement(By.xpath("//*[@id=\"storeLocInp\"]"))
+        driver.findElement(By.id("storeLocInp"))
                 .sendKeys("33031", Keys.ENTER);
         Thread.sleep(500);
         driver.findElement(By.xpath("/html/body/div[8]/div[2]/div[4]/div[3]/div[1]/div/button")).click();
@@ -134,13 +136,13 @@ public class testingFinal {
         Thread.sleep(2500);
 
         // Click fishing category, then go to fishing subcategories (fishing main page)
-        driver.findElement(By.xpath("//*[@id=\"departmentButton_3074457345616732393\"]")).click();
+        driver.findElement(By.id("departmentButton_3074457345616732393")).click();
         Thread.sleep(250);
-        driver.findElement(By.xpath("//*[@id=\"departmentLink_3074457345616732393\"]")).click();
+        driver.findElement(By.id("departmentLink_3074457345616732393")).click();
         Thread.sleep(2500);
 
         // Find and accept cookies warning so that subcategories are clickable
-        driver.findElement(By.xpath("//*[@id=\"CookieTrackingDisclosureButton\"]")).click();
+        driver.findElement(By.id("CookieTrackingDisclosureButton")).click();
 
         // Click power reels category
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[3]/div/div[1]/div")).click();
@@ -150,11 +152,11 @@ public class testingFinal {
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div/div[4]/div[2]/div[1]/div[6]/div/div/div[1]/div[2]/div[1]")).click();
         Thread.sleep(2500);
         exe.executeScript("window.scroll(0,800)","");
-        driver.findElement(By.xpath("//*[@id=\"SKU_List_Widget_Add2CartButton_3074457345623450858_table\"]")).click();
+        driver.findElement(By.id("SKU_List_Widget_Add2CartButton_3074457345623450858_table")).click();
         Thread.sleep(1500);
 
         driver.quit();
-    }
+    } 
 
 
 }
