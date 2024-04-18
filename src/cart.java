8f70e03ce716df92c79b37fa73282419db40c2d0
oki <gtracer465@gmail.com>
2024-04-18 15:12:34 -0400

    Restructure and add 3rd test to cart, storeSelector

--- src/cart.java
+++ src/cart.java
@@ -7,11 +7,13 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class cart {
+    public WebDriver driver;
+    public JavascriptExecutor exe;
     @Test(priority = 1)
     public void addToCart() throws InterruptedException {
         // Init driver
-        WebDriver driver = new FirefoxDriver();
-        JavascriptExecutor exe = (JavascriptExecutor) driver;
+        driver = new FirefoxDriver();
+        exe = (JavascriptExecutor) driver;
         driver.get("http://basspro.com");
         driver.manage().window().maximize();
         Thread.sleep(2500);
@@ -31,46 +33,26 @@ public class cart {
 
         // Click Daiwa Tanacom 1200 Electric Reel
         driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div/div[4]/div[2]/div[1]/div[6]/div/div/div[1]/div[2]/div[1]")).click();
-        Thread.sleep(5000);
+        Thread.sleep(4000);
         driver.findElement(By.xpath("/html/body/div[8]/div[2]/div/div[3]/div[3]/div[7]/div/a")).click();
         driver.findElement(By.id("SKU_List_Widget_Add2CartButton_3074457345623450858_table")).click();
-        Thread.sleep(7500);
-
-        driver.quit();
-    }
-
-    @Test(priority = 2)
-    public void removeFromCart() throws InterruptedException {
-        // Init driver
-        WebDriver driver = new FirefoxDriver();
-        JavascriptExecutor exe = (JavascriptExecutor) driver;
-        driver.get("http://basspro.com");
-        driver.manage().window().maximize();
-        Thread.sleep(2500);
-
-        // First, add item to cart:
-        // Click fishing category, then go to fishing subcategories (fishing main page)
-        driver.findElement(By.id("departmentButton_3074457345616732393")).click();
-        Thread.sleep(250);
-        driver.findElement(By.id("departmentLink_3074457345616732393")).click();
-        Thread.sleep(2500);
-
-        // Find and accept cookies warning so that subcategories are clickable
-        driver.findElement(By.id("CookieTrackingDisclosureButton")).click();
-
-        // Click power reels category
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[3]/div/div[1]/div")).click();
-        Thread.sleep(2500);
-
-        // Click Daiwa Tanacom 1200 Electric Reel, Shop Below, and Add To Cart
-        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div/div[4]/div[2]/div[1]/div[6]/div/div/div[1]/div[2]/div[1]")).click();
-        Thread.sleep(5000);
-        driver.findElement(By.xpath("/html/body/div[8]/div[2]/div/div[3]/div[3]/div[7]/div/a")).click();
-        driver.findElement(By.id("SKU_List_Widget_Add2CartButton_3074457345623450858_table")).click();
-        Thread.sleep(6000);
+        Thread.sleep(4000);
 
         // Navigate to cart via sidepane
         driver.findElement(By.id("GotoCartButton2")).click();
+        Thread.sleep(5000);
+    }
+    @Test(priority = 2)
+    public void changeCartQuantity() throws InterruptedException {
+        // Increment qty by 1
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div[5]/div/div[1]/button")).click();
+        Thread.sleep(2500);
+    }
+
+    @Test(priority = 3)
+    public void removeFromCart() throws InterruptedException {
+        // Decrement quantity
+        driver.findElement(By.xpath("/html/body/div[1]/div[2]/div[3]/div[2]/div/div/div[2]/div[1]/div[2]/div/div/div[2]/div[5]/div/div[1]/button[1]")).click();
         Thread.sleep(2500);
 
         // Click remove
@@ -79,4 +61,5 @@ public class cart {
 
         driver.quit();
     }
+
 }
--- src/storeSelector.java
+++ src/storeSelector.java
@@ -12,11 +12,13 @@ import org.testng.annotations.Test;
  */
 public class storeSelector {
 
+    public WebDriver driver;
+    public JavascriptExecutor exe;
     @Test(priority = 1)
     public void selectFtMyers() throws InterruptedException {
         // Init driver
-        WebDriver driver = new FirefoxDriver();
-        JavascriptExecutor exe = (JavascriptExecutor) driver;
+        driver = new FirefoxDriver();
+        exe = (JavascriptExecutor) driver;
         driver.get("http://basspro.com");
         driver.manage().window().maximize();
         Thread.sleep(2500);
@@ -30,7 +32,29 @@ public class storeSelector {
         driver.findElement(By.xpath("/html/body/div[8]/div[2]/div[4]/div[3]/div[1]/div/button")).click();
         Thread.sleep(4000);
 
-        driver.quit();
     }
 
+    @Test(priority = 2)
+    public void selectPSL() throws InterruptedException{
+        // Click store selector, input zip code, save store
+        driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div[1]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.id("storeLocInp"))
+                .sendKeys("34984", Keys.ENTER);
+        Thread.sleep(500);
+        driver.findElement(By.xpath("/html/body/div[8]/div[2]/div[4]/div[3]/div[1]/div/button")).click();
+        Thread.sleep(4000);
+    }
+
+    @Test(priority = 3)
+    public void selectFtLauderdale() throws InterruptedException {
+        // Click store selector, input zip code, save store
+        driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[2]/div[1]")).click();
+        Thread.sleep(500);
+        driver.findElement(By.id("storeLocInp"))
+                .sendKeys("33301", Keys.ENTER);
+        Thread.sleep(500);
+        driver.findElement(By.xpath("/html/body/div[8]/div[2]/div[4]/div[3]/div[1]/div/button")).click();
+        Thread.sleep(4000);
+    }
 }
