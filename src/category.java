b04c314b85cc5f21a81c279800b480376597d041
oki <gtracer465@gmail.com>
2024-04-18 15:23:29 -0400

    Add addtl test to category class

--- src/category.java
+++ src/category.java
@@ -5,15 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
-/* TODO --------------------------
-    Add tests to go to different category main pages
- */
-
 public class category {
+    public WebDriver driver;
+
     @Test(priority = 1)
-    public void categoryTest() throws InterruptedException {
+    public void categoryDropdownTest() throws InterruptedException {
         // Init driver
-        WebDriver driver = new FirefoxDriver();
+        driver = new FirefoxDriver();
         driver.get("http://basspro.com");
         driver.manage().window().maximize();
         Thread.sleep(2500);
@@ -21,12 +19,30 @@ public class category {
         // Cycle through four different categories
         driver.findElement(By.id("departmentButton_3074457345616732393")).click(); // Fishing
         Thread.sleep(500);
+        driver.findElement(By.id("departmentButton_3074457345616732410")).click(); // Boating
+        Thread.sleep(500);
+        driver.findElement(By.id("departmentButton_3074457345616732421")).click(); // Shooting
+        Thread.sleep(500);
+        driver.findElement(By.id("departmentButton_3074457345616732432")).click(); // Hunting
+        Thread.sleep(4000);
+    }
+
+    @Test(priority = 2)
+    public void categoryMainPages() throws InterruptedException{
+        // Click fishing category again, then visit Fishing main page
+        driver.findElement(By.id("departmentButton_3074457345616732393")).click();
+        Thread.sleep(500);
+        driver.findElement(By.id("departmentLink_3074457345616732393")).click();
+        Thread.sleep(3000);
+
+        driver.navigate().back();
+        Thread.sleep(2000);
+
+        // Now, click Boating category, and visit main page
         driver.findElement(By.id("departmentButton_3074457345616732410")).click();
         Thread.sleep(500);
-        driver.findElement(By.id("departmentButton_3074457345616732421")).click();
-        Thread.sleep(500);
-        driver.findElement(By.id("departmentButton_3074457345616732432")).click();
-        Thread.sleep(4000);
+        driver.findElement(By.id("departmentLink_3074457345616732410")).click();
+        Thread.sleep(3000);
 
         driver.quit();
     }
